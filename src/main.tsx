6405afa66f9d6bb51d15e08752c2131481126ae5
Aljina12 <aljinarai105@gmail.com>
2026-09-23 20:29:02 +0545

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './pages/Homepage'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>,
-)
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
