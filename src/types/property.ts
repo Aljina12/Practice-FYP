7edc9036d34b18979afd899ac95ae5adf0a6fae8
Aljina12 <aljinarai105@gmail.com>
2026-09-25 21:50:51 +0545

    commit vayo

--- src/types/property.ts
+++ src/types/property.ts
@@ -1,3 +1,14 @@
 export interface Property {
-
+    id: number;
+    name: string;
+    location: string;
+    description: string;
+    image: string;
+    price: number;
+    rating: number;
+    bedrooms: number;
+    guests: number;
+    superhost: boolean;
+    country: string;
+    type: string;
 }
